73b50de4786e8944744305eff49b79229133cd16
Anttoha <167896136+Antoha2012pro@users.noreply.github.com>
2025-05-13 18:01:21 +0200

    3

--- js/functions.js
+++ js/functions.js
@@ -94,10 +94,88 @@
 
 // Нумерація повинна починатися з 1. Наприклад, для першого елемента масиву ['Mango', 'Poly', 'Ajax'] з індексом 0 буде виведено '1 - Mango', а для індексу 2 виведе '3 - Ajax'.
 
-const logItems = function(array) {
-    for(let i = 0; i < array.length; i += 1){
-        console.log(`${i + 1} - ${array[i]}`);
-    }
-    return array;
+// const logItems = function(array) {
+//     for(let i = 0; i < array.length; i += 1){
+//         console.log(`${i + 1} - ${array[i]}`);
+//     }
+//     return array;
+// }
+// console.log(logItems(['Mango', 'Poly', 'Ajax']));
+
+
+
+
+// Урок 2
+
+// const a = "Hello World!";
+// console.log(a);
+// // if (true) {
+// //     const b = 0;
+// // }
+// // console.log(sum(1, 2));
+// const sumElement = sum(2, 2); // Виклик функції і присвоєння до змінної sumElement
+// console.log(sumElement);
+// function sum(a, b) { // Оголошення функції
+//     const res = a + b;
+//     return res;
+// }
+
+// const multiPly = function(a, b) { // Не буде працювати якщо звернення до функції буде вище самої функції
+//     return a * b;
+// }
+// console.log(multiPly(2, 6));
+
+// const pvsevdoArts = function() {
+//     const array = Array.from(arguments); // Конвертує з псевдомасива до обичного масива
+//     console.log(array);
+    
+//     console.log(arguments);
+//     for (let index = 0; index < arguments.length; index++) {
+//         const element = arguments[index];
+//         console.log(element);
+//     }
+// }
+// console.log(pvsevdoArts(1, 5, 10, 30, 50));
+
+// Два однакових приклада
+
+// const pvsevdoArts = function(...args) {
+//     console.log(args);
+//     console.log(arguments);
+//     for (let index = 0; index < args.length; index++) {
+//         const element = args[index];
+//         console.log(element);
+//     }
+// }
+// console.log(pvsevdoArts(1, 5, 10, 30, 50));
+
+
+// Напиши функцію logItems(array), яка отримує масив і використовує цикл for, який для кожного елемента масиву буде виводити в консоль повідомлення в форматі [номер елемента] - [значення елемента].
+
+// Нумерація повинна починатися з 1. Наприклад, для першого елемента масиву ['Mango', 'Poly', 'Ajax'] з індексом 0 буде виведено '1 - Mango', а для індексу 2 виведе '3 - Ajax'.
+
+// const logItems = function(array) {
+//     for (let i = 0; i < array.length; i += 1){
+//         console.log(`${i + 1} - ${array[i]}`);
+//     }
+//     return array;
+// }
+// console.log(logItems(['Mango', 'Poly', 'Ajax']));
+
+
+// Напиши скрипт підрахунку вартості гравіювання прикрас. Для цього створи функцію calculateEngravingPrice(message, pricePerWord) приймаючу рядок (в рядку будуть тільки слова і прогалини) і ціну гравіювання одного слова, і повертає ціну гравіювання всіх слів в рядку.
+const calculateEngravingPrice = function(message, pricePerWord) {
+    const result = message.split(" ");
+    const totalPrice = result.length * pricePerWord;
+    return totalPrice;
 }
-console.log(logItems(['Mango', 'Poly', 'Ajax']));
+
+const stringMessage = "Напиши скрипт підрахунку вартості гравіювання прикрас";
+const price = 5000;
+
+const newMessage = "Lorem ipsum dolor sit amet consectetur adipisicing elit Dolore tempore facilis minus cumque asperiores corporis reprehenderit exercitationem debitis Numquam iusto blanditiis fuga adipisci voluptatibus assumenda modi harum mollitia velit animi sadam?";
+const newPrice = 1000;
+
+console.log(calculateEngravingPrice(stringMessage, price));
+console.log(calculateEngravingPrice(newMessage, newPrice));
+
